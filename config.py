7573990fccf36f37f77423cd06a4063c17d4e9f9
teamdcbots <c98814122@gmail.com>
2024-07-27 19:04:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/0ebd97c30a7bb76a988bd.mp4s"
+    "START_IMG_URL", "https://graph.org/file/0ebd97c30a7bb76a988bd.mp4"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://graph.org/file/59d3d5fbaca41ec09f33d.jpg"
