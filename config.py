ed5743a3ffb9b5454374532b0912e9ad9f751ee2
teamdcbots <c98814122@gmail.com>
2024-07-27 19:16:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/teamdcbots/DCxMUSIC",
+    "https://github.com/vicky0604hello/DCxMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
